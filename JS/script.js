3943450129641532096a3bf35af7ba64efaf4c72
eugeneinie <eugeneinie@outlook.com>
2023-09-19 01:46:13 +0100

    Feat: Answered more questions

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// Question1
+const btn = document.querySelectorAll('button');
+function clickEvent(){
+    alert('Button clicked!');
+}
+
+btn[0].addEventListener('click', clickEvent);
+
+// Question2
+function submitEvent() {
+    let inputValue = document.getElementById('myInput').value;
+    alert(inputValue);
+}
+
+btn[1].addEventListener('click', submitEvent)
+
+// Question3
+function calculateSum(num1, num2) {
+    const sum = num1 + num2;
+    const pEl = document.getElementById('sum');
+    pEl.textContent = sum;
+    return sum;
+}
+
+// Question4
+function myFav() {
+    const myFavE = document.querySelector('.myFav');
+    myFavE.textContent = 'New text here!';
+}
+document.getElementById('changeTextButton').addEventListener('click', myFav );
